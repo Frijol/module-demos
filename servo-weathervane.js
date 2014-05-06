1ae077219af5914500ad572ae0335626bb73494d
Frijol <ifoundthemeaningoflife@gmail.com>
2014-05-05 18:00:14 -0700

    breaking http in various ways, now including request, http, and xmlhttp node modules

--- servo-weathervane.js
+++ servo-weathervane.js
@@ -1,24 +1,57 @@
 var tessel = require('tessel');
 // var servo = require('servo-pca9685').use(tessel.port('A'));
 
-var http = require('http');
+// *** Straight JS With XMLHttpRequest Module ***//
+// var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+// function httpGet(theUrl)
+// {
+//   var xmlHttp = null;
+//
+//   xmlHttp = new XMLHttpRequest();
+//   xmlHttp.open( "GET", theUrl, false );
+//   xmlHttp.send( null );
+//   return xmlHttp.responseText;
+// }
+//
+// var res = httpGet('http://api.openweathermap.org/data/2.5/weather?id=5327684&units=imperial');
+// console.log(res);
+//
+// issue: https://github.com/tessel/beta/issues/298
 
-http.get("http://api.openweathermap.org/data/2.5/weather?id=5327684&units=imperial", function(res) {
-  console.log('http get...')
-  if (res.statusCode == 200) {
-    var body = '';
 
-    res.on('data', function(chunk) {
-      body += chunk;
-      console.log('got a chunk')
-    });
+// *** With Node Http ***//
+// var http = require('http');
+// console.log('http get...');
+// http.get("http://api.openweathermap.org/data/2.5/weather?id=5327684&units=imperial", function(res) {
+//   console.log(res.statusCode)
+//   if (res.statusCode == 200) {
+//     var body = '';
+//
+//     res.on('data', function(chunk) {
+//       body += chunk;
+//       console.log(body);
+//     });
+//
+//     res.on('end', function() {
+//       console.log('got end');
+//       response = JSON.parse(body);
+//       console.log(response.main.temp);
+//     });
+//   } else {
+//     console.log(res.statusCode);
+//   }
+// }).on('error', function(e) {
+//   console.log("Got error: " + e.message);
+// });
+//
+//**** issue: https://github.com/tessel/beta/issues/295
 
-    res.on('end', function() {
-      console.log('got end')
-      response = JSON.parse(body);
-      console.log(response.main.temp);
-    });
-  }
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
-});
+// *** With NPM Package Request ***//
+// var request = require('request');
+// request('http://api.openweathermap.org/data/2.5/weather?id=5327684&units=imperial', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body);
+//   }
+// });
+//
+//**** issue: https://github.com/tessel/beta/issues/296
